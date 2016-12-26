c9914131b1c17f5eaf339d28292906cdf4577b86
eab03 <elizabethbright@Elizabeths-MacBook-Air-2.local>
2016-12-26 13:52:50 -0500

    12.26 Added madlib file

---
+++ python/hello_world.py
@@ -0,0 +1,51 @@
+#age = 30
+#next_age = age + 1
+#birth_month = 'August'
+#birth_stone = 'peridot'
+
+#print('Arielle is ' + str(age) + ' years old now, but she\'ll turn ' + str(next_age) + ' in ' + birth_month + '.')
+#print('Her birth stone is ' + birth_stone + '.')
+
+#name = raw_input('What is your name? ')
+#print('Hi ' + name)
+
+#name = raw_input('What is your name?')
+#age = raw_input('How old are you?')
+#print(name + ' is ' + age + ' years old.')
+
+#age = raw_input('How old are you?')
+#dog_years = int(age) * 7
+#print('You are ' + str(dog_years) + ' years old in dog years.')
+
+#meal_cost = float(raw_input('How much was your meal? '))
+#tip = meal_cost * .2
+#total = meal_cost + tip
+
+#print('You should tip $' + str(tip))
+#print('Your total cost would be $' + str(total))
+
+
+adjective1 = raw_input('Type in an adjective: ')
+adjective2 = raw_input('Another adjective: ')
+adjective3 = raw_input('And another adjective: ')
+adjective4 = raw_input('One more adjective: ')
+plural_animal = raw_input('Plural animal (e.g., puppies): ')
+verb = raw_input('Now a verb: ')
+plural_noun = raw_input('Now a plural noun: ')
+musician_first_name = raw_input('The first name of a woman musician: ')
+beverage = raw_input('A beverage: ')
+plural_body_part = raw_input('And finally, a plural body part: ')
+
+
+print ('There once was a ' + adjective1 + ' programmer named ' + musician_first_name +
+' who wanted to build a ' + adjective2 + ' mobile app to help ' + adjective3 + ' ' + plural_animal + ' find new homes.')
+
+print (musician_first_name + ' stayed up all night, drinking lots of caffeinated ' + beverage + ' as she worked and worked, trying to complete her '
++ adjective4 + ' program.')
+
+print ('Whenever ' + musician_first_name + ' hit a dead end, she would exclaim *' + plural_noun + '*!')
+
+print ('But she was not discouraged, and after a quick break to ' + verb + ' and clear her head, she got back to work.')
+
+print ('By morning, when the sun started to rise, she let out a big yawn and stretched her ' + plural_body_part +
+' Finally, she was done.')
